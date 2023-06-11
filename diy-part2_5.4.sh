145e7207aade64854d476d53fbaae8e0799bd9d8
ywt114 <58467878+ywt114@users.noreply.github.com>
2023-06-11 17:50:12 +0800

    Update diy-part2_5.4.sh

--- diy-part2_5.4.sh
+++ diy-part2_5.4.sh
@@ -45,36 +45,36 @@ curl -fsSL https://raw.githubusercontent.com/ywt114/poweroff/main/system.lua > f
 
 # 删除替换默认源插件
 rm -rf feeds/packages/lang/golang
-svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
+svn co https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
 rm -rf feeds/packages/net/smartdns
-svn export https://github.com/pymumu/openwrt-smartdns/trunk feeds/packages/net/smartdns
+svn co https://github.com/pymumu/openwrt-smartdns/trunk feeds/packages/net/smartdns
 rm -rf feeds/packages/net/adguardhome
-svn export https://github.com/kenzok8/openwrt-packages/trunk/adguardhome feeds/packages/net/adguardhome
+svn co https://github.com/kenzok8/openwrt-packages/trunk/adguardhome feeds/packages/net/adguardhome
 rm -rf feeds/packages/net/socat
-svn export https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/op-socat/socat feeds/packages/net/socat
+svn co https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/op-socat/socat feeds/packages/net/socat
 rm -rf feeds/packages/net/mosdns
-svn export https://github.com/kenzok8/openwrt-packages/trunk/mosdns feeds/packages/net/mosdns
+svn co https://github.com/kenzok8/openwrt-packages/trunk/mosdns feeds/packages/net/mosdns
 rm -rf feeds/packages/utils/v2dat
-svn export https://github.com/kenzok8/openwrt-packages/trunk/v2dat feeds/packages/utils/v2dat
+svn co https://github.com/kenzok8/openwrt-packages/trunk/v2dat feeds/packages/utils/v2dat
 
 # 添加插件
 cd package/lean
-svn export https://github.com/ywt114/luci-app-advanced/trunk ./luci-app-advanced
-svn export https://github.com/pymumu/luci-app-smartdns/branches/lede ./luci-app-smartdns
-svn export https://github.com/sirpdboy/chatgpt-web/trunk ./luci-app-chatgpt
-svn export https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus ./luci-app-ssr-plus
-svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall ./luci-app-passwall
-svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash ./luci-app-openclash
-svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome ./luci-app-adguardhome
-svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-mosdns ./luci-app-mosdns
-svn export https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/op-socat/luci-app-socat ./luci-app-socat
-svn export https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/op-homebox ./op-homebox
-svn export https://github.com/sbwml/luci-app-alist/trunk ./op-alist
-svn export https://github.com/sirpdboy/luci-app-autotimeset/trunk ./luci-app-autotimeset
+svn co https://github.com/ywt114/luci-app-advanced/trunk ./luci-app-advanced
+svn co https://github.com/pymumu/luci-app-smartdns/branches/lede ./luci-app-smartdns
+svn co https://github.com/sirpdboy/chatgpt-web/trunk ./luci-app-chatgpt
+svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus ./luci-app-ssr-plus
+svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall ./luci-app-passwall
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash ./luci-app-openclash
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome ./luci-app-adguardhome
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-mosdns ./luci-app-mosdns
+svn co https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/op-socat/luci-app-socat ./luci-app-socat
+svn co https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/op-homebox ./op-homebox
+svn co https://github.com/sbwml/luci-app-alist/trunk ./op-alist
+svn co https://github.com/sirpdboy/luci-app-autotimeset/trunk ./luci-app-autotimeset
 sed -i 's/control"/system"/g' luci-app-autotimeset/luasrc/controller/autotimeset.lua
-svn export https://github.com/linkease/istore/trunk ./istore
+svn co https://github.com/linkease/istore/trunk ./istore
 sed -i 's/+luci-lib-ipkg/+luci-base/g' istore/luci/luci-app-store/Makefile
-svn export https://github.com/kenzok8/small/trunk ./small
+svn co https://github.com/kenzok8/small/trunk ./small
 rm -rf small/luci-app-bypass
 rm -rf small/luci-app-passwall
 rm -rf small/luci-app-passwall2
