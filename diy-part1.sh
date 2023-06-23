293667984c1c435b1c95343b5cad122be13eaebf
ywt114 <58467878+ywt114@users.noreply.github.com>
2023-06-23 16:27:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,6 +3,7 @@
 
 # 修改feeds源
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i "s/src-git/src-git-full/g" feeds.conf.default
 
 # 添加feeds源
 #echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
